385dda5c87bfb58f531ff976b66b8b13882a9645
yasuomaidana <yasuomaidana@gmail.com>
2024-08-29 21:25:58 +0900

    Applying 2D kernel filtering

--- mask-operations/src/main.rs
+++ mask-operations/src/main.rs
@@ -1,11 +1,32 @@
-use opencv::highgui::named_window;
+use opencv::core::{hconcat, Mat, Vector};
+use opencv::highgui::{imshow, wait_key};
+use opencv::imgcodecs::{imread, IMREAD_COLOR};
+use opencv::imgproc::filter_2d;
 
 fn main() {
-    named_window("window", 0).unwrap();
+    let kernel_values: Vec<f32> = vec![
+        0.0, -1.0, 0.0,
+        -1.0, 5.0, -1.0,
+        0.0, -1.0, 0.0,
+    ];
 
-    loop {
-        if opencv::highgui::wait_key(10).unwrap() > 0 {
-            break;
-        }
-    }
+    // Create a Mat object with the kernel values
+    let kernel = Mat::from_slice_2d(
+        &[&kernel_values[0..3],
+            &kernel_values[3..6],
+            &kernel_values[6..9]]).unwrap();
+    let img = imread("./images/cat.jpeg", IMREAD_COLOR).expect("Image not found");
+    let mut output = Mat::default();
+    filter_2d(&img, &mut output, -1, &kernel, Default::default(), Default::default(), 0).unwrap();
+    let mut comparison = Mat::default();
+
+
+    let mut images:Vector<Mat> = Vector::new();
+    images.push(img);
+    images.push(output);
+
+    hconcat(&images, &mut comparison).expect("Failed to concatenate images");
+
+    imshow("Image", &comparison).expect("Failed to show image");
+    wait_key(0).expect("Failed to wait for key");
 }

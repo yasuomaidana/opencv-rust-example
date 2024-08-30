46c4a9df9b426d8470646d474c1d7ff0cd925339
yasuomaidana <yasuomaidana@gmail.com>
2024-08-30 17:25:07 +0900

    Applying min max without mask and drawing output.

--- operation-with-images/src/main.rs
+++ operation-with-images/src/main.rs
@@ -1,4 +1,4 @@
-use opencv::core::{Mat, MatTraitConst, Size, Vec3b, BORDER_DEFAULT};
+use opencv::core::{min_max_loc, Mat, MatTraitConst, Size, Vec3b, BORDER_DEFAULT, CV_8U};
 use opencv::highgui::{imshow, wait_key};
 use opencv::imgcodecs::{imread, IMREAD_COLOR};
 use opencv::imgproc::{cvt_color, COLOR_BGR2GRAY, gaussian_blur};
@@ -20,5 +20,22 @@ fn main() {
     gaussian_blur(&output,&mut gaussian_output, kernel_size, 25.0, 25.0, BORDER_DEFAULT).unwrap();
     imshow("Gray-Gaussian image", &gaussian_output).unwrap();
 
+    let mut min_val = 0.0;
+    let mut max_val = 0.0;
+    let mut min_point = Default::default();
+    let mut max_point = Default::default();
+    let default_mask = Mat::default();
+
+    min_max_loc(&gaussian_output,
+                Some(&mut min_val), Some(&mut max_val),
+                Some(&mut min_point), Some(&mut max_point),
+                &default_mask).unwrap();
+
+    println!("Min value: {}, Max value: {}", min_val, max_val);
+    println!("Min point: {:?}, Max point: {:?}", min_point, max_point);
+
+    let mut draw = Mat::default();
+    gaussian_output.convert_to(&mut draw, CV_8U, 255.0/(max_val - min_val), -min_val * 255.0/(max_val - min_val)).unwrap();
+    imshow("Final Draw", &draw).unwrap();
     wait_key(0).expect("Failed to wait for key");
 }
